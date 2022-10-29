79cf763ce33df3a33dfa7063af88a6b62ea8bf4a
workkk <gentlebob1998@icloud.com>
2022-10-30 00:01:08 +0800

    feat: string

---
+++ string/string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  using namespace std;
+  string s1 = "";
+  cin >> s1;
+  cout << s1;
+
+  string foo = "Hello";
+  string bar = "world\n";
+
+  cout << foo + bar;
+  return 0;
+}
